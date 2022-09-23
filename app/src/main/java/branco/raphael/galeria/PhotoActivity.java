eb4c977916ae7894269a3e822cf860b7ff36cf64
20201tiimi0403 <raphaelbranco2004@gmail.com>
2022-09-23 10:06:57 -0300

    Parei na página 18

--- app/src/main/java/branco/raphael/galeria/PhotoActivity.java
+++ app/src/main/java/branco/raphael/galeria/PhotoActivity.java
@@ -1,8 +1,14 @@
 package branco.raphael.galeria;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+        //Setando a toolbar (pegando)
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
+        setSupportActionBar(toolbar);
+        //Ativando o botão voltar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        //Método de inflar o menu
+        //Cria as opções de menu definidas
     }
+    //Método de inflar o menu
+    //Cria as opções de menu definidas
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void sharePhoto() {
+    }
+
 }
